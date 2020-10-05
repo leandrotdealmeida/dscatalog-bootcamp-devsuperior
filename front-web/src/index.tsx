d8d971580ab48c320ef936688567604aacb74a0f
Leandro Almeida <leandrotrovilho@gmail.com>
2020-10-05 20:25:37 -0300

    Add Front Web

---
+++ front-web/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReacDOM from 'react-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+
+ReacDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
