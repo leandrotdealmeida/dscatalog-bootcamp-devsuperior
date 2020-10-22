9bc80a7d1bf3bd84598f91d188f53b73b9665e1a
devjapa <leandrotrovilho@gmail.com>
2020-10-21 23:58:04 -0300

    Add Page Product List

--- front-web/src/pages/Admin/components/Products/index.tsx
+++ front-web/src/pages/Admin/components/Products/index.tsx
@@ -1,21 +1,13 @@
 import React from "react";
-import { Link, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import List from './List'
 
 const Products = () => {
   return (
-    <div>
-        <Link to="/admin/products" className="mr-5">
-            Listar produtos
-        </Link>
-        <Link to="/admin/products/create" className="mr-5">
-            Criar produtos
-        </Link>
-        <Link to="/admin/products/10" className="mr-5">
-            Editar produtos
-        </Link>
+    <div>      
       <Switch>
         <Route path="/admin/products" exact>
-          <h1>Exibir a listagem de products</h1>
+          <List />
         </Route>
         <Route path="/admin/products/create">
           <h1>Criar um novo produto</h1>
