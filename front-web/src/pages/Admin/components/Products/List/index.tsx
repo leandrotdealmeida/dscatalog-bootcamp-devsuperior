3b640bca56258d57469768e35bfabc37ac19198f
devjapa <leandrotrovilho@gmail.com>
2020-10-22 19:41:41 -0300

    Add base admin form structure

--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -3,9 +3,11 @@ import { useHistory } from "react-router-dom";
 
 const List = () => {
   const history = useHistory();
+
   const handleCreate = () => {
     history.push("/admin/products/create");
   };
+  
   return (
     <div className="admin-products-list">
       <button className="btn btn-primary btn-lg" onClick={handleCreate}>
