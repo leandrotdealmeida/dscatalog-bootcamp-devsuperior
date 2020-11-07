f68f12b0cb22a6a4f90d013dbf481e6cef26cd30
devjapa <leandrotrovilho@gmail.com>
2020-11-07 12:40:44 -0300

    Login validations

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,5 +1,5 @@
 import ButtonIcon from 'core/components/ButtonIcon';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import AuthCard from '../Card';
@@ -12,22 +12,30 @@ type FormData = {
 }
 
 const Login = () => {
-    const { register, handleSubmit} = useForm<FormData>(); // initialize the hook
-    
+    const { register, handleSubmit } = useForm<FormData>(); // initialize the hook
+    const [hasError, setHasError] = useState(false);
+
     const onSubmit = (data: FormData) => {
         console.log(data);
-        makeLogin(data);
+        makeLogin(data)
+            .then(response => {
+                setHasError(false);
+            })
+            .catch(() => {
+                setHasError(true);
+            })
     };
 
     return (
         <AuthCard title="login">
-            <form className="login-form"  onSubmit={handleSubmit(onSubmit)}>
+
+            <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
                 <input
                     type="email"
                     className="form-control input-base margin-bottom-30"
                     placeholder="Email"
                     name="username"
-                     ref={register} 
+                    ref={register({ required: true })}
                 />
 
                 <input
@@ -35,11 +43,15 @@ const Login = () => {
                     className="form-control input-base"
                     placeholder="Senha"
                     name="password"
-                     ref={register} 
+                    ref={register({ required: true })}
                 />
                 <Link to="/admin/auth/recover" className="login-link-recover">
                     Esqueci a senha?
                 </Link>
+                {hasError && (
+                    <div className="alert alert-danger mt-5" role="alert" >
+                        Usuário ou senhas inválidos!
+                    </div>)}
                 <div className="login-submit">
                     <ButtonIcon text="logar" />
                 </div>
