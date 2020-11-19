1dd4fe2c3e275174bdfe2d3316376c4d970bfe19
devjapa <leandrotrovilho@gmail.com>
2020-11-19 20:02:08 -0300

    Add menu Login and Logout

--- front-web/src/core/utils/request.ts
+++ front-web/src/core/utils/request.ts
@@ -27,7 +27,7 @@ axios.interceptors.response.use(function (response) {
   }, function (error) {
       if (error.response.status === 401) {
           //console.log('capturado');
-          history.push('/admin/auth/login');
+          history.push('/auth/login');
       }    
     return Promise.reject(error);
   });
