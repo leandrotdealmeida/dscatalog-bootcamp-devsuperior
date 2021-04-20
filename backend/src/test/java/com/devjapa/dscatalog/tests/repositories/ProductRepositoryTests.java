4cdb6116a870e1f9eb7e3c1530cdfe882e1bfba1
devjapa <leandrotrovilho@gmail.com>
2021-04-20 12:03:20 -0300

    Novos teste repository

--- backend/src/test/java/com/devjapa/dscatalog/tests/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/devjapa/dscatalog/tests/repositories/ProductRepositoryTests.java
@@ -1,5 +1,7 @@
 package com.devjapa.dscatalog.tests.repositories;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
@@ -10,8 +12,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 
+import com.devjapa.dscatalog.entities.Category;
 import com.devjapa.dscatalog.entities.Product;
 import com.devjapa.dscatalog.repositories.ProductRepository;
 import com.devjapa.dscatalog.tests.factory.ProductFactory;
@@ -26,6 +28,9 @@ public class ProductRepositoryTests {
 	private long nonExistingId;
 	private long countTotalProducts;
 	private long countPcGameProducts;
+	private PageRequest pageRequest;
+	private long countCategoryExits;
+	private long countTotalCategories;
 	
 	@BeforeEach
 	void setUp() throws Exception {
@@ -33,12 +38,65 @@ public class ProductRepositoryTests {
 		nonExistingId = 1000L;
 		countTotalProducts = 25L;
 		countPcGameProducts = 21L;
+		pageRequest =  PageRequest.of(0, 10);
+		countCategoryExits = 1L;
+		countTotalCategories = 3L;
+
+	}
+	
+	@Test
+	public void findShouldReturnProductsWhenCategoriesExists() {
+		List<Category> categories = new ArrayList<>();
+		categories.add(new Category(1L, "Livros"));
+		
+		Page<Product> result = repository.find(categories, "", pageRequest);
+		
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals(countCategoryExits, result.getTotalElements());
+	}
+	
+	@Test
+	public void findShouldReturnNothingWhenCategoryDoesNotExists() {
+		List<Category> categories = new ArrayList<>();
+		categories.add(new Category(1L, ""));
+		
+		Page<Product> result = repository.find(categories, "", pageRequest);
+		
+		Assertions.assertFalse(result.isEmpty());
+	}
+	
+	@Test
+	public void findShouldReturnNothingWhenNameDoesNotExists() {
+		String name = "Camera";
+		
+		Page<Product> result = repository.find(null, name, pageRequest);
+		
+		Assertions.assertTrue(result.isEmpty());
+	}
+	
+	@Test
+	public void findShouldReturnAllProductsWhenNameIsEmpty() {
+		String name = "";
+		
+		Page<Product> result = repository.find(null, name, pageRequest);
+		
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals(countTotalProducts, result.getTotalElements());
+	}
+	
+	@Test
+	public void findShouldReturnProductsWhenNameExistsIgnoringCase() {
+		String name = "pc GaMer";
+		
+		Page<Product> result = repository.find(null, name, pageRequest);
+		
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals(countPcGameProducts, result.getTotalElements());
 	}
 	
 	@Test
 	public void findShouldReturnProductsWhenNameExists() {
 		String name = "PC Gamer";
-		PageRequest pageRequest =  PageRequest.of(0, 10);
 		
 		Page<Product> result = repository.find(null, name, pageRequest);
 		
