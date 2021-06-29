d6e5a94cd9bd605d1ca3a42311681ae8ac8e8e53
devjapa <leandrotrovilho@gmail.com>
2021-06-29 17:30:21 -0300

    # WARNING: head commit changed in the meantime
    feat(Novos testes repository)

---
+++ backend/src/test/java/com/devjapa/dscatalog/tests/services/ProductsServiceTests.java
@@ -0,0 +1,12 @@
+package com.devjapa.dscatalog.tests.services;
+
+import com.devjapa.dscatalog.repositories.ProductRepository;
+import com.devjapa.dscatalog.services.ProductService;
+
+public class ProductsServiceTests {
+	
+	private ProductService service;
+	
+	private ProductRepository repository;
+
+}
